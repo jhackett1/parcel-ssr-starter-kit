846242f9e6bab3277d210ea954f627f4c462bee6
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-07-21 12:52:34 +0100

    added reach router

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import React from "react"
-import { renderToString } from "react-dom/server"
+import { renderToStaticMarkup } from "react-dom/server"
+import { ServerLocation, isRedirect } from "@reach/router"
 import App from "../shared/App"
 import fs from "fs"
 import path from "path"
@@ -12,19 +13,35 @@ const server = express()
 
 server.use(express.static("dist/client"))
 
-server.get("*", (req, res)=>{
-    const context = {}
-    const content = renderToString(<App/>)
-    fs.readFileSync(path.join(__dirname, '..', 'client', 'index.html'), (err, html)=>{
-        if(err) return console.error(error)
-        res.send(html.replace(
-            `<div id="root"></div>`,
-            `<div id="root">${content}</div>`
-        ))
-    })
+console.log
+
+server.get("/", (req, res)=>{
+    let content
+    try {
+      content = renderToStaticMarkup(
+        <ServerLocation url={req.url}>
+          <App/>
+        </ServerLocation>
+      )
+    } catch (e) {
+      if (isRedirect(e))res.redirect(e.uri)
+    }
+
+    res.send("ok")
+
+    // fs.readFileSync(path.join(__dirname, '..', 'client', 'index.html'), (err, html)=>{
+    //     if(err) return console.error(error)
+        
+    //     // console.log(html)
+        
+    //     res.send(
+    //         // `<div id="root"></div>`,
+    //         // `<div id="root">${content}</div>`
+    //     )
+    // })
 })
 
-const port = process.env.PORT || 1234
+const port = process.env.PORT || 3000
 
 server.listen(port, ()=>{
     console.log(`👍  Server running on http://localhost:${port}`)
--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -1,6 +1,21 @@
 import React from "react"
+import { Router, Link } from "@reach/router"
+
+const Home = () =>
+    <h2>Home page</h2>
+
+const Second = () =>
+    <h2>Second page</h2>
 
 const App = () =>
-    <h1>Testinggg</h1>
+<>
+    <h1>My app</h1>
+    <Link to="/">Home</Link>
+    <Link to="/second">Second</Link>
+    <Router>
+        <Home path="/"/>
+        <Second path="/second"/>
+    </Router>
+</>
 
 export default App
