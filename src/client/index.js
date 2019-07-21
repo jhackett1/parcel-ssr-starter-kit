4a51dd23ed128a648b5f29d48177247d645b2127
Jaye <joshua.hackett93@gmail.com>
2019-07-21 14:56:24 +0100

    webpack refactor

--- src/client/index.js
+++ src/client/index.js
@@ -4,9 +4,7 @@ import App from "../shared/App"
 
 // Hydrate only if in prod
 if(process.env.NODE_ENV === "production") {
-    console.log("hydrating")
     hydrate(<App/>, document.getElementById("root"))
 } else {
-    console.log("rendering")
     render(<App/>, document.getElementById("root"))
 }
